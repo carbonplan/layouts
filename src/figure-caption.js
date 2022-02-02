b4dd850f3e951f335102306286ac6fae445628bf
freeman-lab <the.freeman.lab@gmail.com>
2022-02-01 22:31:45 -0800

    add table caption

--- src/figure-caption.js
+++ src/figure-caption.js
@@ -1,27 +1,11 @@
 import React from 'react'
-import { Box } from 'theme-ui'
+import Caption from './caption'
 
 const FigureCaption = ({ number, children }) => {
   return (
-    <Box
-      as='figcaption'
-      sx={{
-        color: 'secondary',
-        mt: [3, 3, 3, 4],
-        mb: [6, 6, 6, 7],
-        fontSize: [2, 2, 2, 3],
-      }}
-    >
-      {number && (
-        <>
-          <Box sx={{ letterSpacing: 'smallcaps', display: 'inline-block' }}>
-            FIGURE {number}
-          </Box>{' '}
-          <Box sx={{ display: 'inline-block', mx: [1], pr: [1] }}>/</Box>
-        </>
-      )}
+    <Caption number={number} label='figure'>
       {children}
-    </Box>
+    </Caption>
   )
 }
 
