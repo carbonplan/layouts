f358bcb2b0337eaf2edbfc830a9ab97be7c2b852
Kata Martin <katasm@gmail.com>
2022-03-21 15:07:26 -0700

    Create figure component

---
+++ src/figure.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Box } from 'theme-ui'
+import { Group } from '@carbonplan/components'
+
+const Figure = ({ as = 'figure', children, sx }) => {
+  return (
+    <Box
+      as={as}
+      sx={{
+        my: [6, 6, 6, 7],
+        '@media print': {
+          breakInside: 'avoid',
+        },
+        ...sx,
+      }}
+    >
+      <Group spacing={[4, 4, 4, 5]}>{children}</Group>
+    </Box>
+  )
+}
+
+export default Figure
