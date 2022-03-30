488ed751fc0dc997a48d769eac00c2015bda76f3
Kata Martin <katasm@gmail.com>
2022-03-30 10:44:35 -0700

    Handle hiding groups of ids

--- src/cite.js
+++ src/cite.js
@@ -56,28 +56,39 @@ const Cite = ({ id, ids, ...props }) => {
     throw Error('array of ids is empty')
   } else if (count === 1) {
     return <CiteInner id={ids[0]} {...props} />
-  } else if (count === 2) {
+  }
+
+  let hide
+  if (Array.isArray(props.hide) && props.hide.length !== count) {
+    throw Error('length of hide must match length of ids')
+  } else if (Array.isArray(props.hide)) {
+    hide = props.hide
+  } else {
+    hide = new Array(count).fill(props.hide)
+  }
+
+  if (count === 2) {
     return (
       <>
-        <CiteInner id={ids[0]} {...props} />
+        <CiteInner id={ids[0]} {...props} hide={hide[0]} />
         <CiteSeparator sep=',' />
-        <CiteInner id={ids[1]} {...props} />
+        <CiteInner id={ids[1]} {...props} hide={hide[1]} />
       </>
     )
   } else if (count === 3) {
     return (
       <>
-        <CiteInner id={ids[0]} {...props} />
+        <CiteInner id={ids[0]} {...props} hide={hide[0]} />
         <CiteSeparator sep=',' />
-        <CiteInner id={ids[1]} {...props} />
+        <CiteInner id={ids[1]} {...props} hide={hide[1]} />
         <CiteSeparator sep=',' />
-        <CiteInner id={ids[2]} {...props} />
+        <CiteInner id={ids[2]} {...props} hide={hide[2]} />
       </>
     )
   } else {
     return (
       <>
-        <CiteInner id={ids[0]} {...props} />
+        <CiteInner id={ids[0]} {...props} hide={hide[0]} />
         {ids.slice(1, count - 1).map((d, i) => (
           <Box as='span' key={d}>
             <CiteSeparator
@@ -88,6 +99,7 @@ const Cite = ({ id, ids, ...props }) => {
               id={d}
               sxLabel={{ display: ['initial', 'initial', 'none', 'none'] }}
               {...props}
+              hide={hide[i + 1]}
             />
           </Box>
         ))}
@@ -99,7 +111,7 @@ const Cite = ({ id, ids, ...props }) => {
           sep='-'
           sx={{ display: ['none', 'none', 'initial', 'initial'] }}
         />
-        <CiteInner id={ids[count - 1]} {...props} />
+        <CiteInner id={ids[count - 1]} {...props} hide={hide[count - 1]} />
       </>
     )
   }
