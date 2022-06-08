e0b82a4689338d99b73d0d517bfc8cdb964840af
Kata Martin <katasm@gmail.com>
2022-06-08 14:22:45 -0700

    Set up commentary layout

---
+++ src/commentary.js
@@ -0,0 +1,190 @@
+import React from 'react'
+import { Box, Text, Container, Divider, Themed } from 'theme-ui'
+import {
+  Layout,
+  Row,
+  Column,
+  Button,
+  LinkGroup,
+  formatDate,
+} from '@carbonplan/components'
+import { Left } from '@carbonplan/icons'
+import { PrintedFootnotes } from './printed-footnotes'
+import { ReferencesProvider } from './references'
+import ReadMore from './read-more'
+
+const prefix = 'https://images.carbonplan.org'
+
+const getSx = (color) => {
+  return {
+    headingBorder: {
+      pt: 3,
+      borderStyle: 'solid',
+      borderWidth: 0,
+      borderTopWidth: [0, '1px'],
+      borderImage: (theme) =>
+        `linear-gradient(to right, ${theme.colors[color]}  15%, transparent 15%) 100% 0`,
+    },
+    heading: {
+      fontFamily: 'mono',
+      letterSpacing: 'mono',
+      textTransform: 'uppercase',
+      color,
+      fontSize: [1, 1, 2, 3],
+    },
+  }
+}
+const Commentary = ({
+  back = '/research',
+  children,
+  meta,
+  references,
+  displayTitle,
+  ...props
+}) => {
+  const sx = getSx(meta.color)
+
+  return (
+    <Layout
+      card={`${prefix}/social/${meta.card}.png`}
+      description={meta.quickLook + '.'}
+      title={meta.title + ' – CarbonPlan'}
+      links={'local'}
+      metadata={'scroll'}
+      nav={'research'}
+      printable={true}
+      {...props}
+    >
+      <Box
+        sx={{
+          px: [0, 0, 0],
+          pt: [0, 0, '100px'],
+        }}
+      >
+        {meta.links && (
+          <Row>
+            <Column start={[1, 2, 4, 4]} width={[6, 6, 6, 6]}>
+              <LinkGroup
+                color='secondary'
+                members={meta.links}
+                sx={{ mb: [5], mt: [6, -2, -2, -2] }}
+              />
+            </Column>
+          </Row>
+        )}
+
+        <Box as='article'>
+          <Row>
+            <Column
+              start={[1, 1, 2, 2]}
+              width={[2]}
+              dr={1}
+              sx={{
+                mb: [-3, '-120px', 0, 0],
+                mt: ['20px', '94px', '109px', '122px'],
+              }}
+            >
+              <Button
+                inverted
+                size='xs'
+                onClick={() => {
+                  if (window.history.state?.idx) {
+                    window.history.back()
+                  } else {
+                    window.location.href = back
+                  }
+                }}
+                prefix={<Left />}
+                sx={{
+                  ml: ['-2px', '-2px', '-2px', '-2px'],
+                  '@media print': {
+                    display: 'none',
+                  },
+                }}
+              >
+                Back
+              </Button>
+            </Column>
+
+            <Column start={[1, 2, 4, 4]} width={[6, 6, 6, 6]}>
+              <Themed.h1>{displayTitle || meta.title}</Themed.h1>
+            </Column>
+          </Row>
+
+          <Row sx={{ mb: [3, 3, 4, 5] }}>
+            <Column
+              start={[1, 1, 2, 2]}
+              width={[3, 1, 2, 2]}
+              sx={{ ...sx.headingBorder, order: [1] }}
+            >
+              <Box
+                sx={{
+                  ...sx.heading,
+                  mt: ['3px'],
+                }}
+              >
+                <Box
+                  as='span'
+                  sx={{ display: ['none', 'initial', 'none', 'none'] }}
+                >
+                  ({meta.number})
+                </Box>
+                <Box
+                  as='span'
+                  sx={{ display: ['initial', 'none', 'initial', 'initial'] }}
+                >
+                  COMMENTARY({meta.number})
+                </Box>
+              </Box>
+            </Column>
+            <Column
+              start={[1, 2, 4, 4]}
+              width={[6, 5, 6, 6]}
+              sx={{ ...sx.headingBorder, order: [3, 2] }}
+            >
+              <Box sx={{ lineHeight: [1.15, 1.15, 1.35, 1.35] }}>
+                <Text sx={sx.heading}>
+                  by{' '}
+                  {meta.authors.map((author, ix) => (
+                    <Text
+                      key={author}
+                      sx={{ display: 'inline-block', mr: [2] }}
+                    >
+                      {author.replace(/ /g, '\u00a0')}{' '}
+                      {ix < meta.authors.length - 1 ? '+' : ''}
+                    </Text>
+                  ))}
+                </Text>
+              </Box>
+            </Column>
+
+            <Column
+              start={[4, 7, 10, 10]}
+              width={[3, 2, 2, 2]}
+              sx={{ ...sx.headingBorder, order: [2, 3] }}
+            >
+              <Text sx={sx.heading}>{formatDate(meta.date)}</Text>
+            </Column>
+          </Row>
+
+          <Row>
+            <Column start={[1, 2, 4, 4]} width={[6, 6, 6, 6]}>
+              <ReferencesProvider color={meta.color} references={references}>
+                {children}
+
+                <PrintedFootnotes />
+              </ReferencesProvider>
+
+              <Divider
+                sx={{ mt: [6, 6, 7, 7], '@media print': { display: 'none' } }}
+              />
+              <ReadMore target='research' />
+            </Column>
+          </Row>
+        </Box>
+      </Box>
+    </Layout>
+  )
+}
+
+export default Commentary
