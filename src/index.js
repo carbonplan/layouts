794d1ebb82b1d547c492a64c063809f45363d459
katamartin <katasm@gmail.com>
2023-03-09 15:57:02 -0800

    Export SidebarDivider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 export { default as Article } from './article'
 export { default as Cite } from './cite'
 export { default as Commentary } from './commentary'
-export { Sidebar, SidebarAttachment, SidebarFooter } from './sidebar'
+export {
+  Sidebar,
+  SidebarAttachment,
+  SidebarDivider,
+  SidebarFooter,
+} from './sidebar'
 export { default as Endnote } from './endnote'
 export { default as NavSection } from './nav-section'
 export { default as NavMenu } from './nav-menu'
--- src/sidebar/index.js
+++ src/sidebar/index.js
@@ -1,3 +1,4 @@
 export { default as Sidebar } from './sidebar'
 export { default as SidebarAttachment } from './sidebar-attachment'
 export { default as SidebarFooter } from './sidebar-footer'
+export { default as SidebarDivider } from './sidebar-divider'
